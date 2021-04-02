f026db2eb05d1db73bce3a790969d0216216f255
Sébastien Levy <sebastien.levy@cdbdx.biz>
2021-04-02 11:27:38 +0200

    testing
    - changed module resolution for the ts engine in the test suite
    - prepared test suite for chai usage

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,19 @@
-import * as assert from 'assert';
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+
+const assert = chai.assert;
+const suite = mocha.suite;
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as cppLlvmCoverage from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
 });
