748f06fa099f263e3434285b24f86bc3c30b166a
Sébastien Levy <sebastien.levy@cdbdx.biz>
2021-04-06 16:16:16 +0200

    configuration
    - more precise specification of settings in package.json

--- src/cppLlvmCoverage.ts
+++ src/cppLlvmCoverage.ts
@@ -1,7 +1,2 @@
-interface Settings { }
-
 export class CppLlvmCoverage {
-    private _settings: Settings;
-
-    constructor(settings: Settings) { this._settings = settings; }
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,37 +1,14 @@
 import * as chai from 'chai';
 import * as mocha from 'mocha';
 
-const assert = chai.assert;
-const should = chai.should();
-const suite = mocha.suite;
 const describe = mocha.describe;
-const it = mocha.it;
+// const it = mocha.it;
+// const should = chai.should();
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import { CppLlvmCoverage } from '../../cppLlvmCoverage';
+// import * as vscode from 'vscode';
+// import { CppLlvmCoverage } from '../../cppLlvmCoverage';
 
-class FakeSettingsForWrongCMakeSetting {
-  private cmakeCommand: string = "";
-
-  constructor() {
-    throw new Error("foo");
-  }
-}
-
-suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
-
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
-
-});
-
-describe('Extension behavior regarding its configuration', () => {
-  it('Should output an error regarding the cmake command when the setting is empty', () => {
-    should.throw(() => { new FakeSettingsForWrongCMakeSetting(); }, "");
-  });
+describe('Extension behavior regarding extension settings', () => {
 });
