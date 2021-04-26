5d7604c9f1c9746f6953d8d0ededda39b2013bd9
MetaBarj0 <troctsch.cpp@gmail.com>
2021-04-26 22:05:05 +0200

    unit testing
    - differentiated cases where input stream is empty or not

--- src/adapters/streamedUncoveredCodeRegionsCollector.ts
+++ src/adapters/streamedUncoveredCodeRegionsCollector.ts
@@ -8,9 +8,21 @@ export class StreamedUncoveredCodeRegionsCollector implements UncoveredCodeRegio
   }
 
   collectUncoveredCodeRegions(): Promise<void> {
-    return Promise.reject(
-      'Cannot collect any missing coverage information. Input is empty.\n' +
-      'Ensure the cmake target you specified in settings lead to the creation of a coverage information file.');
+    return new Promise((_, reject) => {
+      this.inputStream.on("readable", () => {
+        if (this.inputStream.readableLength === 0)
+          return reject(
+            'Cannot collect any missing coverage information. Input is empty.\n' +
+            'Ensure the cmake target you specified in `cmake-llvm-coverage: Cmake Target` setting lead to the creation ' +
+            'of a coverage information file.');
+
+        return reject(
+          'Cannot collect any missing coverage information. Input is not a json document.\n' +
+          'Ensure the file you specified in `cmake-llvm-coverage: Coverage Info File Name` setting ' +
+          'target a json file containing coverage information.'
+        );
+      });
+    });
   }
 
   private readonly inputStream: Readable;
--- tests/suites/unit/uncoveredCodeRegionsCollector.test.ts
+++ tests/suites/unit/uncoveredCodeRegionsCollector.test.ts
@@ -7,7 +7,7 @@ chai.should();
 
 import { StreamedUncoveredCodeRegionsCollector } from '../../../src/adapters/streamedUncoveredCodeRegionsCollector';
 
-import { Readable } from 'stream';
+import { Readable, ReadableOptions } from 'stream';
 
 describe('UncoveredCodeRegionsCollector behavior', () => {
   it('should throw an exception when attempting to collect uncovered code region if the input stream is empty', () => {
@@ -15,10 +15,30 @@ describe('UncoveredCodeRegionsCollector behavior', () => {
 
     return collector.collectUncoveredCodeRegions().should.eventually.be.rejectedWith(
       'Cannot collect any missing coverage information. Input is empty.\n' +
-      'Ensure the cmake target you specified in settings lead to the creation of a coverage information file.');
+      'Ensure the cmake target you specified in `cmake-llvm-coverage: Cmake Target` setting lead to the creation ' +
+      'of a coverage information file.');
   });
+
+  it('should throw an exception when attempting to collect uncovered code region if the input stream ' +
+    'does not contain a json document.', () => {
+      const collector = new StreamedUncoveredCodeRegionsCollector(buildNotJsonStream());
+
+      return collector.collectUncoveredCodeRegions().should.eventually.be.rejectedWith(
+        'Cannot collect any missing coverage information. Input is not a json document.\n' +
+        'Ensure the file you specified in `cmake-llvm-coverage: Coverage Info File Name` setting ' +
+        'target a json file containing coverage information.');
+    });
 });
 
 function buildEmptyInputStream(): Readable {
-  return new class extends Readable { };
+  return new class extends Readable {
+    constructor(options?: ReadableOptions) {
+      super(options);
+      this.push(null);
+    }
+  };
+}
+
+function buildNotJsonStream(): Readable {
+  return Readable.from('foo');
 }
