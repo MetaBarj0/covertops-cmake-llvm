25f4df7b915419c86a29811ea5f43598842bf97b
MetaBarj0 <troctsch.cpp@gmail.com>
2021-04-29 17:59:40 +0200

    cmake
    - refacto, using mocks of child process

--- tests/suites/unit/cmake.test.ts
+++ tests/suites/unit/cmake.test.ts
@@ -2,7 +2,8 @@ import * as chai from 'chai';
 import { describe, it } from 'mocha';
 import * as chaiAsPromised from 'chai-as-promised';
 
-import { VscodeWorkspaceConfigurationLike, VscodeWorkspaceLike } from '../../../src/domain/services/settings-provider';
+import { SettingsProvider, VscodeUriLike, VscodeWorkspaceConfigurationLike, VscodeWorkspaceFolderLike, VscodeWorkspaceLike } from '../../../src/domain/services/settings-provider';
+import * as path from 'path';
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -16,27 +17,69 @@ describe('the behavior of the cmake internal service used to build the target ' 
 
         const cmake = new Cmake(workspace, process);
 
-        cmake.buildTarget().should.eventually.be.rejectedWith(
+        return cmake.buildTarget().should.eventually.be.rejectedWith(
           "Cannot find the cmake command. Ensure the 'cmake-llvm-coverage Cmake Command' " +
           'setting is correctly set. Have you verified your PATH environment variable?');
       });
   });
 
-type ProcessLike = {};
+type ExecFileOptionsLike = {
+  cwd?: string;
+  env?: NodeJS.ProcessEnv;
+};
+
+type ExecFileExceptionLike = {
+  message: string;
+};
+
+type ChildProcessLike = {};
+
+type ProcessLike = {
+  execFile(
+    file: string,
+    args: ReadonlyArray<string> | undefined | null,
+    options: ExecFileOptionsLike,
+    callback: (error: ExecFileExceptionLike | null, stdout: string, stderr: string) => void
+  ): ChildProcessLike;
+};
 
 class Cmake {
-  constructor(_workspace: VscodeWorkspaceLike, _process: ProcessLike) { }
+  constructor(workspace: VscodeWorkspaceLike, process: ProcessLike) {
+    this.process = process;
+    this.workspace = workspace;
+  }
 
   buildTarget(): Promise<void> {
-    return Promise.reject(new Error(
-      "Cannot find the cmake command. Ensure the 'cmake-llvm-coverage Cmake Command' " +
-      'setting is correctly set. Have you verified your PATH environment variable?'));
+    return new Promise((resolve, reject) => {
+      const settings = new SettingsProvider(this.workspace).settings;
+      const cmakeCommand = settings.cmakeCommand;
+
+      this.process.execFile(
+        cmakeCommand, ['--version'], {},
+        (error, _stdout, _stderr) => {
+          if (error)
+            return reject(new Error(
+              "Cannot find the cmake command. Ensure the 'cmake-llvm-coverage Cmake Command' " +
+              'setting is correctly set. Have you verified your PATH environment variable?'));
+
+          resolve();
+        });
+    });
   }
+
+  private readonly process: ProcessLike;
+  private readonly workspace: VscodeWorkspaceLike;
 };
 
 function buildFakeWorkspace() {
   return new class implements VscodeWorkspaceLike {
-    workspaceFolders = undefined;
+    workspaceFolders = [
+      new class implements VscodeWorkspaceFolderLike {
+        uri = new class implements VscodeUriLike {
+          fsPath = path.resolve('root', 'workspace');
+        };
+      }
+    ];
 
     getConfiguration(_section?: string) {
       return new class implements VscodeWorkspaceConfigurationLike {
@@ -47,5 +90,18 @@ function buildFakeWorkspace() {
 }
 
 function buildFakeProcessForWrongCmakeCommand() {
-  return new class implements ProcessLike { };
+  return new class implements ProcessLike {
+    execFile(file: string,
+      _args: readonly string[] | null | undefined,
+      _options: ExecFileOptionsLike,
+      callback: (error: ExecFileExceptionLike | null, stdout: string, stderr: string) => void) {
+      callback(new class implements ExecFileExceptionLike {
+        message = `${file}: command not found.`;
+      },
+        'stdout',
+        'stderr');
+
+      return new class implements ChildProcessLike { };
+    }
+  };
 }
