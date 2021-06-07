c9cc1ecefe374e73ab6a95f0d3dfb2fd1846d9a5
MetaBarj0 <troctsch.cpp@gmail.com>
2021-06-07 14:39:31 +0200

    refacto
    - inferred extension's definitions from package.json

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,4 @@
-// TODO: refacto by deduplicating by importing json
-export const extensionNameInSettings = "Cmake-llvm-coverage";
-export const extensionId = "cmake-llvm-coverage";
+import * as packageJSON from '../package.json';
+
+export const extensionNameInSettings = packageJSON.contributes.configuration[0].title;
+export const extensionId = packageJSON.name;
