a287137c52ebc4a02da0de3f057219ca322b785d
MetaBarj0 <troctsch.cpp@gmail.com>
2021-06-11 10:28:59 +0200

    refacto
    - enhanced readability of coverage info collector unit test suite

--- tests/suites/unit/coverage-info-collector.test.ts
+++ tests/suites/unit/coverage-info-collector.test.ts
@@ -13,8 +13,27 @@ import { vscodeWorkspace as v } from '../../faked-adapters/vscode-workspace';
 import { inputStream as i } from '../../faked-adapters/input-stream';
 import { globbing as g } from '../../faked-adapters/globbing';
 
+describe('Unit test suite', () => {
+  describe('the coverage info collector behavior', () => {
+    describe('with invalid input readable streams', () => {
+      describe('collect coverage info summary', shouldFailToCollectCoverageInfoSummaryBecauseOfInvalidStream);
+      describe('collect uncovered region coverage info', shouldFailToCollectUncoveredRegionsBecauseOfInvalidStream);
+    });
+    describe('with a valid input readable stream', () => {
+      describe('for an unhandled source file', () => {
+        describe('collect coverage info summary', shouldFailToCollectCoverageInfoSummaryBecauseOfUnhandledSourceFile);
+        describe('collect uncovered region coverage info', shouldFailToCollectUncoveredRegionsBecauseOfUnhandledSourceFile);
+      });
+      describe('for a handled source file', () => {
+        describe('collect coverage info summary', shouldSucceedToCollectCoverageInfoSummary);
+        describe('collect uncovered region coverage info', shouldSucceedToCollectUncoveredRegions);
+      });
+    });
+  });
+});
+
 // TODO(WIP): enhance tests readability
-describe('The collection of coverage summary and uncovered code regions with an invalid input readable stream', () => {
+function shouldFailToCollectCoverageInfoSummaryBecauseOfInvalidStream() {
   [
     i.buildEmptyReadableStream,
     i.buildInvalidLlvmCoverageJsonObjectStream,
@@ -38,7 +57,10 @@ describe('The collection of coverage summary and uncovered code regions with an 
           'settings are correctly set.');
     });
   });
+}
 
+function shouldFailToCollectUncoveredRegionsBecauseOfInvalidStream() {
+  // TODO: duplication of the array
   [
     i.buildEmptyReadableStream,
     i.buildInvalidLlvmCoverageJsonObjectStream,
@@ -64,9 +86,9 @@ describe('The collection of coverage summary and uncovered code regions with an 
           'settings are correctly set.');
     });
   });
-});
+}
 
-describe('The collection of coverage summary and uncovered code regions with a valid input readable stream', () => {
+function shouldFailToCollectCoverageInfoSummaryBecauseOfUnhandledSourceFile() {
   it('should fail to provide coverage summary for an unhandled source file', async () => {
     const collector = CoverageInfoCollector.make({
       globSearch: g.buildFakeGlobSearchForExactlyOneMatch(),
@@ -82,7 +104,28 @@ describe('The collection of coverage summary and uncovered code regions with a v
       .should.eventually.be.rejectedWith('Cannot find any summary coverage info for the file ' +
         `${sourceFilePath}. Ensure this source file is covered by a test in your project.`);
   });
+}
 
+function shouldFailToCollectUncoveredRegionsBecauseOfUnhandledSourceFile() {
+  it('should fail to provide uncovered code regions for an unhandled source file', async () => {
+    // TODO: duplication in arrange
+    const collector = CoverageInfoCollector.make({
+      globSearch: g.buildFakeGlobSearchForExactlyOneMatch(),
+      workspace: v.buildFakeWorkspaceWithWorkspaceFolderAndOverridableDefaultSettings(),
+      llvmCoverageInfoStreamBuilder: i.buildFakeStreamBuilder(i.buildValidLlvmCoverageJsonObjectStream)
+    });
+
+    const sourceFilePath = '/an/unhandled/source/file.cpp';
+    const coverageInfo = await collector.collectFor(sourceFilePath);
+    const iterateOnUncoveredRegions = async () => { for await (const _region of coverageInfo.uncoveredRegions()); };
+
+    return iterateOnUncoveredRegions()
+      .should.eventually.be.rejectedWith('Cannot find any uncovered code regions for the file ' +
+        `${sourceFilePath}. Ensure this source file is covered by a test in your project.`);
+  });
+}
+
+function shouldSucceedToCollectCoverageInfoSummary() {
   it('should succeed in provided summary coverage info for handled source file', async () => {
     const collector = CoverageInfoCollector.make({
       globSearch: g.buildFakeGlobSearchForExactlyOneMatch(),
@@ -99,23 +142,9 @@ describe('The collection of coverage summary and uncovered code regions with a v
     summary.notCovered.should.be.equal(0);
     summary.percent.should.be.equal(100);
   });
+}
 
-  it('should fail to provide uncovered code regions for an unhandled source file', async () => {
-    const collector = CoverageInfoCollector.make({
-      globSearch: g.buildFakeGlobSearchForExactlyOneMatch(),
-      workspace: v.buildFakeWorkspaceWithWorkspaceFolderAndOverridableDefaultSettings(),
-      llvmCoverageInfoStreamBuilder: i.buildFakeStreamBuilder(i.buildValidLlvmCoverageJsonObjectStream)
-    });
-
-    const sourceFilePath = '/an/unhandled/source/file.cpp';
-    const coverageInfo = await collector.collectFor(sourceFilePath);
-    const iterateOnUncoveredRegions = async () => { for await (const _region of coverageInfo.uncoveredRegions()); };
-
-    return iterateOnUncoveredRegions()
-      .should.eventually.be.rejectedWith('Cannot find any uncovered code regions for the file ' +
-        `${sourceFilePath}. Ensure this source file is covered by a test in your project.`);
-  });
-
+function shouldSucceedToCollectUncoveredRegions() {
   it('should succeed to provide uncovered regions for a handled source file', async () => {
     const collector = CoverageInfoCollector.make({
       globSearch: g.buildFakeGlobSearchForExactlyOneMatch(),
@@ -146,4 +175,4 @@ describe('The collection of coverage summary and uncovered code regions with a v
       }
     });
   });
-});
+}
