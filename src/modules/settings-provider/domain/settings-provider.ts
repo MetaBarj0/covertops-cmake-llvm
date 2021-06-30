8f8b8db174f22a7153e963c139cd34fdf0b173d3
MetaBarj0 <troctsch.cpp@gmail.com>
2021-06-30 15:28:20 +0200

    refacto
    - even more experiencing with imports

--- src/modules/settings-provider/domain/settings-provider.ts
+++ src/modules/settings-provider/domain/settings-provider.ts
@@ -1,13 +1,12 @@
 import * as Imports from '../imports';
-import { OutputChannelLike, VscodeWorkspaceFolderLike, VscodeWorkspaceLike } from '../../../shared-kernel/abstractions/vscode';
 
 export function make(adapters: Adapters) {
   return new SettingsProvider(adapters);
 }
 
 type Adapters = {
-  workspace: VscodeWorkspaceLike,
-  errorChannel: OutputChannelLike
+  workspace: Imports.Abstractions.Adapters.vscode.VscodeWorkspaceLike,
+  errorChannel: Imports.Abstractions.Adapters.vscode.OutputChannelLike
 };
 
 // TODO: make an abstraction on that provider?
@@ -17,14 +16,14 @@ class SettingsProvider {
     this.errorChannel = adapters.errorChannel;
   }
 
-  get settings(): Imports.Abstractions.Settings {
+  get settings(): Imports.Abstractions.Domain.Settings {
     this.ensureWorkspaceIsLoaded();
 
     const workspaceSettings = this.workspace.getConfiguration(Imports.Extension.definitions.extensionId);
-    const workspaceFolders = this.workspace.workspaceFolders as Array<VscodeWorkspaceFolderLike>;
+    const workspaceFolders = this.workspace.workspaceFolders as Array<Imports.Abstractions.Adapters.vscode.VscodeWorkspaceFolderLike>;
     const rootDirectory = workspaceFolders[0].uri.fsPath;
 
-    return Imports.Implementations.Settings.make(
+    return Imports.Implementations.Domain.Settings.make(
       workspaceSettings.get('cmakeCommand') as string,
       workspaceSettings.get('buildTreeDirectory') as string,
       workspaceSettings.get('cmakeTarget') as string,
@@ -45,6 +44,6 @@ class SettingsProvider {
     throw new Error(errorMessage);
   }
 
-  private readonly workspace: VscodeWorkspaceLike;
-  private readonly errorChannel: OutputChannelLike;
+  private readonly workspace: Imports.Abstractions.Adapters.vscode.VscodeWorkspaceLike;
+  private readonly errorChannel: Imports.Abstractions.Adapters.vscode.OutputChannelLike;
 }
--- src/modules/settings-provider/imports.ts
+++ src/modules/settings-provider/imports.ts
@@ -1,16 +1,29 @@
 import * as _definitions from '../../extension/definitions';
 import { Settings as SettingsContract } from './abstractions/domain/settings';
 import * as SettingModule from './domain/settings';
+import * as _vscode from '../../shared-kernel/abstractions/vscode';
 
 export namespace Extension {
   export const definitions = _definitions;
 }
 
 export namespace Abstractions {
-  export type Settings = SettingsContract;
+  export namespace Domain {
+    export type Settings = SettingsContract;
+  }
+
+  export namespace Adapters {
+    export namespace vscode {
+      export type VscodeWorkspaceLike = _vscode.VscodeWorkspaceLike;
+      export type OutputChannelLike = _vscode.OutputChannelLike;
+      export type VscodeWorkspaceFolderLike = _vscode.VscodeWorkspaceFolderLike;
+    }
+  }
 }
 
 export namespace Implementations {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  export const Settings = SettingModule;
+  export namespace Domain {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    export const Settings = SettingModule;
+  }
 }
