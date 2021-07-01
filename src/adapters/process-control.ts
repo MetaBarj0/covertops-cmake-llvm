1a95102b525d120ab769544858cc026cf69839b3
MetaBarj0 <troctsch.cpp@gmail.com>
2021-07-01 15:46:03 +0200

    refacto
    - reworked process control adapters imports

--- src/adapters/process-control.ts
+++ src/adapters/process-control.ts
@@ -1,5 +1,5 @@
-import { ExecFileCallable } from '../shared-kernel/abstractions/process-control';
+import * as Abstractions from '../shared-kernel/abstractions/process-control';
 
 import * as cp from 'child_process';
 
-export const execFile: ExecFileCallable = cp.execFile;
+export const execFile: Abstractions.ExecFileCallable = cp.execFile;
